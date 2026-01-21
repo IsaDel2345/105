b7078aae33fe3e6e2ba247c917a436d719ce47d9
IsaDel2345 <sviridovaveronika820@gmail.com>
2026-01-21 19:53:31 +0300

    Create task1_19.cs

---
+++ task1_19.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("а) -10");
+        Console.WriteLine("б) -35");
+        Console.WriteLine("в) 0");
+        Console.WriteLine("г) -25");
+    }
+}
