2d9fec9c5d2ae08d15d3445ccf3a4eb3689d1afc
IsaDel2345 <sviridovaveronika820@gmail.com>
2026-01-21 19:50:18 +0300

    Create task1_17.cs

---
+++ task1_17.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("a) Math.Sqrt(x1*x1 + x2*x2)");
+        Console.WriteLine("б) x1*x2 + x1*x3 + x2*x3");
+        Console.WriteLine("в) v0*t + a*t*t/2");
+        Console.WriteLine("г) m*v*v/2 + m*g*h");
+        Console.WriteLine("д) 1/R1 + 1/R2");
+        Console.WriteLine("е) m*g*Math.Cos(alpha)");
+        Console.WriteLine("ж) 2*Math.PI*R");
+        Console.WriteLine("з) b*b - 4*a*c");
+        Console.WriteLine("и) gamma*m1*m2/(r*r)");
+        Console.WriteLine("к) I*I*R");
+        Console.WriteLine("л) a*b*Math.Sin(c)");
+        Console.WriteLine("м) Math.Sqrt(a*a+b*b-2*a*b*Math.Cos(c))");
+        Console.WriteLine("н) (a*d+b*c)/(a*d)");
+        Console.WriteLine("о) Math.Sqrt(1 - Math.Pow(Math.Sin(x),2))");
+        Console.WriteLine("п) 1/Math.Sqrt(a*x*x + b*x + c)");
+        Console.WriteLine("р) (Math.Sqrt(x+1)+Math.Sqrt(x-1))/(2*Math.Sqrt(x))");
+        Console.WriteLine("с) Math.Abs(x)+Math.Abs(x+1)");
+        Console.WriteLine("т) Math.Abs(1-Math.Abs(x))");
+    }
+}
