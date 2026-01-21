4c1406bbe9a5563e9e012e04f0196c34c60a9e8f
IsaDel2345 <sviridovaveronika820@gmail.com>
2026-01-21 19:49:00 +0300

    Create task1_15.cs

---
+++ task1_15.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("a) -1/(x*x)");
+        Console.WriteLine("б) a/(b*c)");
+        Console.WriteLine("в) (a*c)/b");
+        Console.WriteLine("г) (a+b)/2");
+        Console.WriteLine("д) 5.45*(a+2*b)/(2-a)");
+        Console.WriteLine("е) (-b+Math.Sqrt(b*b-4*a*c))/(2*a)");
+        Console.WriteLine("ж) (-b+1/a)/(2/c)");
+        Console.WriteLine("з) 1/(1+(a+b)/2)");
+        Console.WriteLine("и) 1/(1+1/(2+1/(2+3.0/5.0)))");
+        Console.WriteLine("к) Math.Pow(2, m*n)");
+    }
+}
