9da31c9ffed1f183c3fb389e4b378681c6030467
IsaDel2345 <sviridovaveronika820@gmail.com>
2026-01-21 19:48:06 +0300

    Create task1_14.cs

---
+++ task1_14.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("a) 2*x");
+        Console.WriteLine("б) Math.Sin(x)");
+        Console.WriteLine("в) a*a");
+        Console.WriteLine("г) Math.Sqrt(x)");
+        Console.WriteLine("д) Math.Abs(n)");
+        Console.WriteLine("е) 5*Math.Cos(y)");
+        Console.WriteLine("ж) -7.5*a*a");
+        Console.WriteLine("з) 3*Math.Sqrt(x)");
+        Console.WriteLine("и) Math.Sin(alpha)*Math.Cos(beta)+Math.Cos(alpha)*Math.Sin(beta)");
+        Console.WriteLine("к) a*Math.Sqrt(2)*b");
+        Console.WriteLine("л) 3*Math.Sin(2*alpha)*Math.Cos(3*beta)");
+        Console.WriteLine("м) -5*Math.Sqrt(x+Math.Sqrt(y))");
+    }
+}
