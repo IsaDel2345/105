cfa686edf277d6ca9bdaff2d0cf49fa9f5d9033f
IsaDel2345 <sviridovaveronika820@gmail.com>
2026-01-21 19:43:59 +0300

    Create task1_8.cs

---
+++ task1_8.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Globalization;
+
+class Program
+{
+    static double RD() => double.Parse(Console.ReadLine()!.Replace(',', '.'), CultureInfo.InvariantCulture);
+
+    static void Main()
+    {
+        double a = RD(), b = RD(), c = RD(), d = RD();
+        Console.WriteLine($"{a} {b} {c} {d}");
+    }
+}
