cb2f672787d8af3065744e8b21ec42b9e491b648
IsaDel2345 <sviridovaveronika820@gmail.com>
2026-01-22 11:42:31 +0300

    Create task2_12.cs

---
+++ task2_12.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        int x = int.Parse(Console.ReadLine()!);
+        int a = x / 100, b = (x / 10) % 10, c = x % 10;
+        Console.WriteLine(c * 100 + b * 10 + a);
+    }
+}
